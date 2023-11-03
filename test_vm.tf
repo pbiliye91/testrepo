8e0f5d82b53a2965185057c8a3f9b0c1219d5fd3
Vishal Azure <vishal@sunilkumarpalhotmail.onmicrosoft.com>
2023-11-03 09:24:23 +0000

    Updated test_vm.tf

--- test_vm.tf
+++ test_vm.tf
@@ -137,7 +137,7 @@ resource "azurerm_virtual_machine_extension" "my_dsc_extension" {
 
   settings = jsonencode({
     configuration = {
-      "url": "https://samlinkstorage1234.blob.core.windows.net/test-vm/dsc.ps1"
+      "url": "https://samlinkstorage1234.blob.core.windows.net/test-vm/dsc.zip"
       "script": "dsc.ps1"
       function = "InstallMSMQTriggers"
     }
